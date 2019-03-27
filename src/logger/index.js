1a74b68a757f6fe635e25395bdf738ab86f04f48
Nathan Walters <nwalters512@gmail.com>
2019-03-27 13:24:08 -0500

    Allow printing errors with logger

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,22 +1,27 @@
 const winston = require('winston')
+const chalk = require('chalk')
 
 const { format } = winston
 
-const outputFormat = format.printf(({ timestamp, level, message }) => {
-  return `[${timestamp}] ${level}: ${message}`
+const outputFormat = format.printf(info => {
+  let message = `${chalk.bold(`[${info.timestamp}]`)} ${chalk.bold(
+    info.level
+  )}: ${info.message}`
+  if (info.stack) {
+    message += `\n${info.stack}`
+  }
+  return message
 })
 
 const logger = winston.createLogger({
   level: 'info',
-  transports: [
-    new winston.transports.Console({
-      format: format.combine(
-        format.colorize(),
-        format.timestamp(),
-        outputFormat
-      ),
-    }),
-  ],
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.colorize(),
+    format.timestamp(),
+    outputFormat
+  ),
+  transports: [new winston.transports.Console()],
 })
 
 module.exports = logger
