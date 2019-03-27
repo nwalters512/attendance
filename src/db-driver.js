08a94ac475a9954d081638f2b7faf7e86b33caec
Rahul Rameshbabu <robotrahul123@gmail.com>
2019-03-27 01:00:28 -0500

    [psqlDriver] Built basic db-driver module that wraps PrairieLib sql-db driver

--- src/db-driver.js
+++ src/db-driver.js
@@ -1,4 +1,6 @@
-const { Pool } = require('pg')
+const sqlLoader = require('@prairielearn/prairielib/sql-loader')
+const sqlDB = require('@prairielearn/prairielib/sql-db')
+const ERR = require('async-stacktrace')
 const config = require('./lib/config.js')
 const logger = require('./logger')
 const fs = require('fs')
@@ -13,52 +15,38 @@ const pgConf = {
   database: config.postgresqlDatabase,
 }
 
-const pool = new Pool(pgConf)
+const logError = function(err) {
+  if (err) {
+    console.error('Error due to dbDriver', err.stack)
+  }
+}
+
+sqlDB.init(pgConf, logError, logError)
 
 dbDriver.initDB = function() {
   var sqlPath_ = config.sqlFilePath
   var sqlInitFiles_ = config.sqlInitFiles
+  var initFileList_ = []
   sqlInitFiles_.forEach(files => {
     files.forEach(file => {
-      var initFile_ = sqlPath_ + file + '.sql'
-      //var initSql_ = sqlLoader.loadSqlEquiv(initFile_)
-      var query_ = fs.readFileSync(initFile_, 'utf8').toString()
-      pool.query(query_, (err, result) => {
-        if (err) {
-          logger.error('Error executing init query', err.stack)
-        }
-      })
-      //pool.query(initSql_, undefined, logInfo)
+      initFileList_.push(file)
     })
   })
-}
-
-/* TODO PrairieLib interface code, need to debug later
-//const sqlLoader = require('@prairielearn/prairielib/sql-loader')
-//const sqlDB = require('@prairielearn/prairielib/sql-db')
-
-//sqlDB.init(pgConf, logError, logError)
-
-const logError = function(err) {
-    // TODO Figure out how to handle these later
-    //logger.error(err)
-}
-
-const logInfo = function(info) {
-    // TODO Figure out how to handle these later
-    //logger.info(info)
-}
-
-/*
-dbDriver.initDB = function() {
-    var sqlPath_ = config.sqlFilePath
-    var sqlInitFiles_ = config.sqlInitFiles
-    sqlInitFiles_.forEach((files) => {
-        files.forEach((file) => {
-            var initFile_ = sqlPath_ + file + '.sql'
-            var initSql_ = sqlLoader.loadSqlEquiv(initFile_)
-            sqlDB.query(initSql_, {}, logInfo)
-        })
+  var initHelper = fList => {
+    if (fList.length === 0) {
+      logger.info('Finished initializing database')
+      return
+    }
+    var initFile_ = sqlPath_ + fList[0] + '.sql'
+    var initSql_ = sqlLoader.loadSqlEquiv(initFile_)
+    sqlDB.query(initSql_['all'], {}, err => {
+      if (err) logError(err)
+      initHelper(fList.slice(1))
     })
+  }
+  initHelper(initFileList_)
 }
-*/
+
+dbDriver.query = sqlDB.query
+dbDriver.queryOneRow = sqlDB.queryOneRow
+dbDriver.queryWithZeroOrOneRow = sqlDB.queryWithZeroOrOneRow
