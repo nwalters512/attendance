2d283b41d0db02d36100638050ea6467ee669b51
Rahul Rameshbabu <robotrahul123@gmail.com>
2019-03-27 21:13:25 -0500

    Switching over to desktop

--- src/pages/course/course.js
+++ src/pages/course/course.js
@@ -1,8 +1,22 @@
 const router = require('express').Router({ mergeParams: true })
 
 router.get('/', (req, res, _next) => {
-  res.locals.courseId = req.params.courseId
-  res.render(__filename.replace(/\.js/, '.ejs'), res.locals)
+    res.locals.courseId = req.params.courseId
+    // TODO add param
+    sqlDb.query(sql.select_course_instances, [], (err, result) => {
+        if (ERR(err, next)) return
+        res.locals.course_instances = result.rows
+        res.render(__filename.replace(/\.js$/, '.ejs'), res.locals)
+    })
+})
+
+router.post('/', (req, res, next) => {
+    if (req.body.___action === 'newCourseInstance') {
+        const params = {
+            term: req.body.term,
+            name: req.body.
+        }
+    }
 })
 
 module.exports = router
