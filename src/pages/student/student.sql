6e473f5ed15671d8149bce28968915f91d82c0ca
Rahul Rameshbabu <robotrahul123@gmail.com>
2019-04-14 10:54:51 -0500

    Initial commit to demonstrate difference between csv-parse and csv-parse-sync

--- src/pages/student/student.sql
+++ src/pages/student/student.sql
@@ -5,3 +5,10 @@ SELECT * FROM students;
 UPDATE students
 SET firstName = ($fName), lastName = ($lName), netid = ($netid), major = ($major)
 WHERE id = $id
+
+-- BLOCK insert_update_roster
+/* INSERT INTO students ()
+SELECT * FROM UNNEST ()
+ON CONFLICT () DO UPDATE
+    SET
+*/ 
