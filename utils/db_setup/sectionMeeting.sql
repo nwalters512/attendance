0a87a4ec951b83dd2d04643aa17447b413457ae1
Rahul Rameshbabu <robotrahul123@gmail.com>
2019-04-15 00:00:37 -0500

    [schema] Patched froeign key constraint to be per course instance

--- utils/db_setup/sectionMeeting.sql
+++ utils/db_setup/sectionMeeting.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS section_meetings (
   s_ci_term              char(6),
   s_ci_name              varchar(80),
   s_ci_year              smallint,
-  PRIMARY KEY (m_name, s_name),
+  PRIMARY KEY (m_name, s_name, s_ci_term, s_ci_name, s_ci_year),
   FOREIGN KEY (m_name, m_ci_term, m_ci_name, m_ci_year) REFERENCES meetings (name, ci_term, ci_name, ci_year) ON DELETE CASCADE,
   FOREIGN KEY (s_name, s_ci_term, s_ci_name, s_ci_year) REFERENCES sections (name, ci_term, ci_name, ci_year) ON DELETE CASCADE
 );
--- utils/db_setup/swipe.sql
+++ utils/db_setup/swipe.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS swipes (
   sec_name     varchar(80),
   PRIMARY KEY (UIN, meeting_name, sec_name),
   FOREIGN KEY (UIN, stu_ci_term, stu_ci_name, stu_ci_year) REFERENCES students (UIN, ci_term, ci_name, ci_year) ON DELETE CASCADE,
-  FOREIGN KEY (meeting_name, sec_name) REFERENCES section_meetings (m_name, s_name) ON DELETE CASCADE
+  FOREIGN KEY (meeting_name, sec_name, stu_ci_term, stu_ci_name, stu_ci_year) REFERENCES section_meetings (m_name, s_name, s_ci_term, s_ci_name, s_ci_year) ON DELETE CASCADE
 );
