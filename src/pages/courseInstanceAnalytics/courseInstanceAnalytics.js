a3f841e48c6538af4383ad94359ccafadd6fd913
Piotr <0000piotr@gmail.com>
2019-04-17 17:46:16 -0500

    Added first super kewl pie chart for course intance analytics; added d3js supporting static file

--- src/pages/courseInstanceAnalytics/courseInstanceAnalytics.js
+++ src/pages/courseInstanceAnalytics/courseInstanceAnalytics.js
@@ -31,6 +31,27 @@ router.get(
     )).rows
     res.locals.students = students
 
+    const numCS = (await dbDriver.asyncQuery(sql.select_count_students_in_cs, {
+      // ci_term: courseInstance.term,
+      // ci_name: courseInstance.name,
+      // ci_year: courseInstance.year,
+    })).rows[0].num_cs_in_ci
+    res.locals.students = students
+
+    const numNonCS = (await dbDriver.asyncQuery(
+      sql.select_count_students_not_in_cs,
+      {
+        // ci_term: courseInstance.term,
+        // ci_name: courseInstance.name,
+        // ci_year: courseInstance.year,
+      }
+    )).rows[0].num_not_cs_in_ci
+    const content = [
+      { label: 'CS', value: numCS, color: '#44b9ae' },
+      { label: 'Non-CS', value: numNonCS, color: '#0a6097' },
+    ]
+    res.locals.content = content
+
     res.render(__filename.replace(/\.js/, '.ejs'), res.locals)
   })
 )
