2df0551691098f9f3d4327df102414d6561f053b
Piotr <0000piotr@gmail.com>
2019-04-17 21:09:19 -0500

    Fixed camelCase

--- src/pages/courseInstanceAnalytics/courseInstanceAnalytics.sql
+++ src/pages/courseInstanceAnalytics/courseInstanceAnalytics.sql
@@ -23,12 +23,16 @@ SELECT SUM(majorAggregate.enrollment) AS num_cs_in_ci
     FROM students
     INNER JOIN course_instances
     ON students.ci_term = course_instances.term
-    AND students.ci_year = course_instances.year
-    AND students.ci_name = course_instances.name
+      AND students.ci_year = course_instances.year
+      AND students.ci_name = course_instances.name
     INNER JOIN courses
     ON courses.name = course_instances.course_name
+    WHERE (course_instances.term = $ci_term
+      AND course_instances.name = $ci_name
+      AND course_instances.year = $ci_year)
     GROUP BY students.major) majorAggregate
   WHERE major = 'CS';
+
 
 -- BLOCK select_count_students_not_in_cs
 SELECT SUM(majorAggregate.enrollment) AS num_not_cs_in_ci
@@ -37,9 +41,44 @@ SELECT SUM(majorAggregate.enrollment) AS num_not_cs_in_ci
     FROM students
     INNER JOIN course_instances
     ON students.ci_term = course_instances.term
-    AND students.ci_year = course_instances.year
-    AND students.ci_name = course_instances.name
+      AND students.ci_year = course_instances.year
+      AND students.ci_name = course_instances.name
     INNER JOIN courses
     ON courses.name = course_instances.course_name
+    WHERE (course_instances.term = $ci_term
+      AND course_instances.name = $ci_name
+      AND course_instances.year = $ci_year)
     GROUP BY students.major) majorAggregate
   WHERE major <> 'CS' OR major IS NULL;
+
+-- BLOCK select_avg_attendance_rate_not_in_cs
+SELECT (SELECT AVG(nonMajorAttendance.attendance) AS avg_attendence_rate_not_cs_in_ci
+  FROM
+    (SELECT COUNT(*) AS attendance
+      FROM students
+      INNER JOIN section_meetings
+      ON section_meetings.m_ci_term = students.ci_term
+        AND section_meetings.m_ci_name = students.ci_name
+        AND section_meetings.m_ci_year = students.ci_year
+      WHERE (course_instances.term = $ci_term
+        AND course_instances.name = $ci_name
+        AND course_instances.year = $ci_year)
+      GROUP BY major
+    HAVING major <> 'CS' OR major IS NULL) nonMajorAttendance) / COUNT(*) AS avg_num_non_majors_per_section_meeting
+  FROM section_meetings
+
+  -- BLOCK select_avg_attendance_rate_in_cs
+  SELECT (SELECT AVG(nonMajorAttendance.attendance) AS avg_attendence_rate_cs_in_ci
+    FROM
+      (SELECT COUNT(*) AS attendance
+        FROM students
+        INNER JOIN section_meetings
+        ON section_meetings.m_ci_term = students.ci_term
+          AND section_meetings.m_ci_name = students.ci_name
+          AND section_meetings.m_ci_year = students.ci_year
+        WHERE (course_instances.term = $ci_term
+          AND course_instances.name = $ci_name
+          AND course_instances.year = $ci_year)
+        GROUP BY major
+      HAVING major = 'CS') majorAttendance) / COUNT(*) AS avg_num_majors_per_section_meeting
+    FROM section_meetings
