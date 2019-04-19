c0d3626267ee93d9a9482a2f32e2cd4d49488d94
Piotr <0000piotr@gmail.com>
2019-04-19 07:03:57 -0500

    Commented out third graph. Maybe fixed others stuff? idr anymore

--- utils/db_setup/sectionMeeting.sql
+++ utils/db_setup/sectionMeeting.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS section_meetings (
-  id                     serial UNIQUE,
-  m_name                 varchar(80),
+  id                   serial UNIQUE,
+  m_name               varchar(80),
   ci_term              char(6),
   ci_name              varchar(80),
   ci_year              smallint,
-  s_name                 varchar(80),
+  s_name               varchar(80),
   PRIMARY KEY (m_name, s_name, ci_term, ci_name, ci_year),
   FOREIGN KEY (m_name, ci_term, ci_name, ci_year) REFERENCES meetings (name, ci_term, ci_name, ci_year) ON DELETE CASCADE,
   FOREIGN KEY (s_name, ci_term, ci_name, ci_year) REFERENCES sections (name, ci_term, ci_name, ci_year) ON DELETE CASCADE
