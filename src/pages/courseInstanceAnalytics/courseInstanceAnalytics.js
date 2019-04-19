137d28839f78a7aa467bedb23279dbf0785e72cd
Piotr <33298959+PiotrGalusza@users.noreply.github.com>
2019-04-19 08:30:16 -0500

    Data viz testing (#38)
    * Added first super kewl pie chart for course intance analytics; added d3js supporting static file
    
    * Fixed camelCase
    
    * Second viz created - compares avg attendence rates. Next steps: generalize categories to accept all existing majors (not CS/non-CS), ie. SQL query returns (major, count). Same deal for average attendence rates. Also, students get double counted for attending multiple section meetings of the same lab, so avg attendence gets wrecked. Need to change SQL query to only select distinct students from each sec-met
    
    * Bar graph improved: SQL query fixed and generalized, height display and aesthetics fixed. Spacing improved
    
    * Pie chart SQL query and visualizations have been geenralized properly
    
    * Changes?
    
    * Commented out third graph. Maybe fixed others stuff? idr anymore
    
    * Added first super kewl pie chart for course intance analytics; added d3js supporting static file
    
    * Fixed camelCase
    
    * Second viz created - compares avg attendence rates. Next steps: generalize categories to accept all existing majors (not CS/non-CS), ie. SQL query returns (major, count). Same deal for average attendence rates. Also, students get double counted for attending multiple section meetings of the same lab, so avg attendence gets wrecked. Need to change SQL query to only select distinct students from each sec-met
    
    * Bar graph improved: SQL query fixed and generalized, height display and aesthetics fixed. Spacing improved
    
    * Pie chart SQL query and visualizations have been geenralized properly
    
    * Changes?
    
    * Commented out third graph. Maybe fixed others stuff? idr anymore
    
    * some line changes

--- src/pages/courseInstanceAnalytics/courseInstanceAnalytics.js
+++ src/pages/courseInstanceAnalytics/courseInstanceAnalytics.js
@@ -3,6 +3,7 @@ const { sqlLoader } = require('@prairielearn/prairielib')
 const dbDriver = require('../../dbDriver')
 const asyncErrorHandler = require('../../asyncErrorHandler')
 const checks = require('../../auth/checks')
+const colorPallete = require('./colorPallete')
 
 const sql = sqlLoader.loadSqlEquiv(__filename)
 
@@ -30,6 +31,45 @@ router.get(
       }
     )).rows
     res.locals.students = students
+
+    const numPerMajor = (await dbDriver.asyncQuery(
+      sql.select_count_students_by_major,
+      {
+        ci_term: courseInstance.term,
+        ci_name: courseInstance.name,
+        ci_year: courseInstance.year,
+      }
+    )).rows
+
+    const pieContent = []
+    numPerMajor.forEach(element => {
+      pieContent.push({
+        label: element.major,
+        value: element.enrollment,
+        color: colorPallete[Math.floor(Math.random() * colorPallete.length)],
+      })
+    })
+    res.locals.pieContent = pieContent
+
+    const barContent = (await dbDriver.asyncQuery(
+      sql.select_avg_attendance_rate_by_major,
+      {
+        ci_term: courseInstance.term,
+        ci_name: courseInstance.name,
+        ci_year: courseInstance.year,
+      }
+    )).rows
+    res.locals.barContent = barContent
+
+    const lineContent = (await dbDriver.asyncQuery(
+      sql.select_swipetimes_per_section_meeting,
+      {
+        ci_term: courseInstance.term,
+        ci_name: courseInstance.name,
+        ci_year: courseInstance.year,
+      }
+    )).rows
+    res.locals.lineContent = lineContent
 
     res.render(__filename.replace(/\.js/, '.ejs'), res.locals)
   })
--- src/pages/courseInstanceAnalytics/courseInstanceAnalytics.sql
+++ src/pages/courseInstanceAnalytics/courseInstanceAnalytics.sql
@@ -15,3 +15,63 @@ WHERE
   ci_term = $ci_term
   AND ci_name = $ci_name
   AND ci_year = $ci_year;
+
+-- BLOCK select_count_students_by_major
+SELECT (CASE WHEN major IS NULL THEN 'UNKNOWN' ELSE major END) AS major, ( COUNT((CASE WHEN major IS NULL THEN 'UNKNOWN' ELSE major END))) AS enrollment FROM students
+  INNER JOIN course_instances
+  ON students.ci_term = course_instances.term
+    AND students.ci_year = course_instances.year
+    AND students.ci_name = course_instances.name
+  INNER JOIN courses
+  ON courses.name = course_instances.course_name
+  WHERE (course_instances.term = $ci_term
+    AND course_instances.name = $ci_name
+    AND course_instances.year = $ci_year)
+  GROUP BY major;
+
+-- BLOCK select_avg_attendance_rate_by_major
+(SELECT (CASE WHEN major IS NULL THEN 'UNKNOWN' ELSE major END) AS major, 100*( COUNT((CASE WHEN major IS NULL THEN 'UNKNOWN' ELSE major END))*1.0 / (SELECT COUNT(*) FROM meetings
+  WHERE (meetings.ci_term = $ci_term
+    AND meetings.ci_name = $ci_name
+    AND meetings.ci_year = $ci_year) ) ) AS attendencerate
+  FROM students
+    INNER JOIN section_meetings
+      ON section_meetings.ci_term = students.ci_term
+        AND section_meetings.ci_name = students.ci_name
+        AND section_meetings.ci_year = students.ci_year
+    INNER JOIN swipes
+      ON swipes.stu_ci_term = students.ci_term
+        AND swipes.stu_ci_name = students.ci_name
+        AND swipes.stu_ci_year = students.ci_year
+        AND swipes.UIN = students.UIN
+        AND swipes.meeting_name = section_meetings.m_name
+        AND swipes.sec_name = section_meetings.s_name
+    WHERE (section_meetings.ci_term = $ci_term
+      AND section_meetings.ci_name = $ci_name
+      AND section_meetings.ci_year = $ci_year)
+    GROUP BY major);
+
+-- BLOCK select_swipetimes_per_section_meeting
+SELECT CONCAT(sec_name, ' ', meeting_name) AS sectionmeeting, swipe_timestamp AS studentswipe FROM swipes
+  WHERE (swipes.stu_ci_term = $ci_term
+    AND swipes.stu_ci_name = $ci_name
+    AND swipes.stu_ci_year = $ci_year);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
